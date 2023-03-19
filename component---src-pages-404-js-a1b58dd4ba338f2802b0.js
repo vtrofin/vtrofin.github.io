83ea55274598809a1160c174be5c0424f0c0b7da
Victor Trofin <victor_trofin@yahoo.com>
2023-03-19 12:38:13 +0900

    Updates

---
+++ component---src-pages-404-js-a1b58dd4ba338f2802b0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvtrofin_github_io=self.webpackChunkvtrofin_github_io||[]).push([[883],{429:function(e,t,n){n.r(t);var u=n(7294);t.default=()=>u.createElement("div",null,u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
+//# sourceMappingURL=component---src-pages-404-js-a1b58dd4ba338f2802b0.js.map
