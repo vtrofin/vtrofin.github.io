88eea2281ad252ca542d0a51e68affb8b922e4b9
Victor Trofin <victor_trofin@yahoo.com>
2024-02-05 21:31:41 +0900

    feat (about): update profile

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,19 @@
 import React from "react";
+import { differenceInYears, formatDuration } from "date-fns";
 
 const About = ({ size }) => {
   const className = size ? `about-container ${size}` : "about-container";
+  const today = new Date();
+  const init = new Date(2018, 1, 1);
+  const years = differenceInYears(today, init);
+  const duration = formatDuration({ years });
+
   return (
     <div className={className}>
       <p>
-        Versatile full-stack web engineer with over 5 years in Japan's SaaS
-        space, adept in the JavaScript ecosystem, leveraging diverse tech stacks
-        to revolutionize user experiences.
+        Experienced full-stack web engineer with over {duration} of expertise.
+        Proficient in the JavaScript ecosystem, I leverage diverse tech stacks
+        to innovate user experiences.
       </p>
     </div>
   );
