09d4370ee0a618a987ca018dbd482c81f50beb1b
Victor Trofin <victor_trofin@yahoo.com>
2024-05-18 23:13:21 +0900

    chore: fix type errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,10 @@
-const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development';
-require('dotenv').config({ path: `.env.${activeEnv}` });
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development";
+require("dotenv").config({ path: `.env.${activeEnv}` });
 console.log(`Using environment config: '${activeEnv}'`);
 
 module.exports = {
-  pathPrefix: '/',
+  pathPrefix: "/",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     phone: process.env.PHONE,
