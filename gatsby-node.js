a92187862bc053ef79ab88fe3f84af64ae91de89
Victor Trofin <victor_trofin@yahoo.com>
2024-05-18 21:43:29 +0900

    feat: add ts

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,34 +4,19 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
-// exports.modifyWebpackConfig = ({ config, stage }) => {
-//   switch (stage) {
-//     case "develop":
-//       config.merge({
-//         devServer: {
-//             host: "0.0.0.0",
-//             disableHostCheck: true,
-//         }
-//     });
+const path = require("path");
 
-//       break;
-
-//     case "build-css":
-
-//       break;
-
-//     case "build-html":
-
-
-//       break;
-
-//     case "build-javascript":
-
-
-//       break;
-//   }
-
-//   return config;
-// };
-
+exports.onCreateWebpackConfig = ({ actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        "@components": path.resolve(__dirname, "src/components"),
+        "@pages": path.resolve(__dirname, "src/pages"),
+        "@styles": path.resolve(__dirname, "src/styles"),
+        "@types": path.resolve(__dirname, "src/types"),
+      },
+      extensions: [".ts", ".tsx", ".js", ".jsx"],
+    },
+  });
+};
