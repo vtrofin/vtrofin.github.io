1f6bdea7c77ca06fe91b6d497af4c863482a56c0
Victor Trofin <victor_trofin@yahoo.com>
2026-04-11 23:56:11 +0900

    drop agent sniffing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,32 +21,18 @@ const IndexPage: React.FunctionComponent<PageProps<DataProps>> = (props) => {
     () => new URLSearchParams(props.location.search).get("western") === "true",
     [props.location.search],
   );
-  const [pageBreakClass, setPageBreakClass] = useState("page-break");
   const [isBot, setIsBot] = useState(false);
-  const [_isSafari, setIsSafari] = useState(false); // used to generate page breaks for print medium
 
   const email = isBot ? "" : props.data.site.siteMetadata.email;
   const phone = isBot ? "" : props.data.site.siteMetadata.phone;
 
   useEffect(() => {
     const agent = window.navigator.userAgent;
-    const isFirefox = /firefox/gi.test(agent);
-    const isChrome = /chrome/gi.test(agent);
-    const isSafari = /safari/gi.test(agent);
     const bot =
       /bot|googlebot|crawler|spider|robot|crawling|google|baidu|bing|msn|teoma|slurp|yandex/i.test(
         agent,
       );
     setIsBot(bot);
-
-    if (isFirefox) {
-      setPageBreakClass((prev) => prev + " firefox");
-    } else if (isChrome) {
-      setPageBreakClass((prev) => prev + " chrome");
-    } else if (!isChrome && isSafari) {
-      setIsSafari(true);
-      setPageBreakClass((prev) => prev + " safari");
-    }
   }, []);
 
   return (
@@ -70,9 +56,8 @@ const IndexPage: React.FunctionComponent<PageProps<DataProps>> = (props) => {
       <CompetencyGrid items={skills} subtitleText="Abilities" />
       <CompetencyGrid items={technicalSkills} subtitleText="Technical Skills" />
       <EmploymentSection jobs={jobs} subtitleText="Employment history" />
-      {/* <div className={pageBreakClass + " bottom"} /> */}
-      {/* {isSafari && <div className={pageBreakClass + " top"} />} */}
-      {/* {isSafari && <div className={pageBreakClass + " bottom"} />} */}
+      {/* <div className={"page-break" + " bottom"} /> */}
+      {/* {<div className={"page-break" + " top"} />} */}
       <EducationSection education={education} subtitleText="Education" />
     </Layout>
   );
